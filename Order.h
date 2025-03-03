d39649bf5e83f1918414d5a378a64cb277d16e1d
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:53:43 +0400

    class Order

---
+++ Order.h
@@ -0,0 +1,27 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "Dish.h"
+#include "Customer.h"
+#include <vector>
+
+class Order {
+private:
+    Customer* customer;
+    std::vector<Dish*> orderedDishes;
+    double totalPrice;
+
+public:
+    Order(Customer* customer);
+    Order(const Order& other);
+    Order& operator=(const Order& other);
+    Order(Order&& other) noexcept;
+    Order& operator=(Order&& other) noexcept;
+    ~Order() = default;
+
+    void addDish(Dish* dish);
+    void calculateTotal();
+    void displayOrder() const;
+};
+
+#endif // ORDER_H
