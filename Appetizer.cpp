c247cd80678cea93aa55f88f80a329e8ad7d366b
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:42:41 +0400

    function implementations for Appetizer

---
+++ Appetizer.cpp
@@ -0,0 +1,12 @@
+#include "Appetizer.h"
+
+Appetizer::Appetizer(std::string dishName, double dishPrice, bool spicy)
+    : Dish(dishName, dishPrice), isSpicy(spicy) {}
+
+void Appetizer::display() const {
+    std::cout << name << " - $" << price;
+    if (isSpicy) {
+        std::cout << " (Spicy)";
+    }
+    std::cout << std::endl;
+}
