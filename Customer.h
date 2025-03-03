b60ea17ae4fa4e9aec90e682f4e8efd194eb3177
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:52:20 +0400

    class Customer

---
+++ Customer.h
@@ -0,0 +1,27 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+#include <vector>
+#include "Order.h"
+
+class Customer {
+private:
+    std::string name;
+    std::string contactInfo;
+    std::vector<Order> orderHistory;
+
+public:
+    Customer(std::string customerName, std::string contact);
+    Customer(const Customer& other);
+    Customer& operator=(const Customer& other);
+    Customer(Customer&& other) noexcept;
+    Customer& operator=(Customer&& other) noexcept;
+    ~Customer() = default;
+
+    void placeOrder(Order order);
+    void viewOrderHistory() const;
+    std::string getName() const;
+};
+
+#endif // CUSTOMER_H
