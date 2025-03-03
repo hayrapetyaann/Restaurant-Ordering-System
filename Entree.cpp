a3dacda3b7354027c9a40ef54a97751d5b21a522
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:44:23 +0400

    Function implementations for Entree

---
+++ Entree.cpp
@@ -0,0 +1,8 @@
+#include "Entree.h"
+
+Entree::Entree(std::string dishName, double dishPrice, int cal)
+    : Dish(dishName, dishPrice), calories(cal) {}
+
+void Entree::display() const {
+    std::cout << name << " - $" << price << " (" << calories << " kcal)" << std::endl;
+}
