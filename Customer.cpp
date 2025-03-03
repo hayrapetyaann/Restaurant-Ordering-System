dd26ca60ed9edfbb4e0391d203760a19fb04d7c9
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:53:11 +0400

    Function implementations for Customer

---
+++ Customer.cpp
@@ -0,0 +1,46 @@
+#include "Customer.h"
+#include "Order.h"
+#include <iostream>
+
+Customer::Customer(std::string customerName, std::string contact)
+    : name(customerName), contactInfo(contact) {}
+
+Customer::Customer(const Customer& other)
+    : name(other.name), contactInfo(other.contactInfo), orderHistory(other.orderHistory) {}
+
+Customer& Customer::operator=(const Customer& other) {
+    if (this != &other) {
+        name = other.name;
+        contactInfo = other.contactInfo;
+        orderHistory = other.orderHistory;
+    }
+    return *this;
+}
+
+Customer::Customer(Customer&& other) noexcept
+    : name(std::move(other.name)), contactInfo(std::move(other.contactInfo)),
+      orderHistory(std::move(other.orderHistory)) {}
+
+Customer& Customer::operator=(Customer&& other) noexcept {
+    if (this != &other) {
+        name = std::move(other.name);
+        contactInfo = std::move(other.contactInfo);
+        orderHistory = std::move(other.orderHistory);
+    }
+    return *this;
+}
+
+void Customer::placeOrder(Order order) {
+    orderHistory.push_back(order);
+}
+
+void Customer::viewOrderHistory() const {
+    std::cout << "Order history for " << name << ":\n";
+    for (const auto& order : orderHistory) {
+        order.displayOrder();
+    }
+}
+
+std::string Customer::getName() const {
+    return name;
+}
