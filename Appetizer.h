5e54068edfc31c4d5fe1fdda103e77432df9b4fe
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:41:58 +0400

    Appetizer class

---
+++ Appetizer.h
@@ -0,0 +1,14 @@
+#ifndef APPETIZER_H
+#define APPETIZER_H
+
+#include "Dish.h"
+
+class Appetizer : public Dish {
+private:
+    bool isSpicy;
+public:
+    Appetizer(std::string dishName, double dishPrice, bool spicy);
+    void display() const override;
+};
+
+#endif // APPETIZER_H
