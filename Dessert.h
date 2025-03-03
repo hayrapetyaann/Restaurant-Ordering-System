bc7591b80f5475757d25cb6d82af1813b364fad2
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-03 21:45:08 +0400

    Class Dessert

---
+++ Dessert.h
@@ -0,0 +1,14 @@
+#ifndef DESSERT_H
+#define DESSERT_H
+
+#include "Dish.h"
+
+class Dessert : public Dish {
+private:
+    bool containsNuts;
+public:
+    Dessert(std::string dishName, double dishPrice, bool nuts);
+    void display() const override;
+};
+
+#endif // DESSERT_H
