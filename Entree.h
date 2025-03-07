d5c39ac69136380dc7a2b88a30b4b2a8e30308d4
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2025-03-07 15:11:18 +0400

    add reference for string parameter

--- Entree.h
+++ Entree.h
@@ -7,7 +7,7 @@ class Entree : public Dish {
 private:
     int calories;
 public:
-    Entree(std::string dishName, double dishPrice, int cal);
+    Entree(std::string& dishName, double dishPrice, int cal);
     void display() const override;
 };
 
